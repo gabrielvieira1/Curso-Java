aa98e7a0999038c411cb05dc48b8f087176b0d95
Gabriel Vieira <gabrielvieira1@protonmail.com>
2020-04-13 22:52:13 -0300

    StringBuilder

---
+++ Curso Java/src/sessao13/ExercicioResolvido2/Entities/Comment.java
@@ -0,0 +1,24 @@
+package sessao13.ExercicioResolvido2.Entities;
+
+public class Comment {
+	private String text;
+	
+	public Comment() {
+		
+	}
+	
+	public Comment(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+}
