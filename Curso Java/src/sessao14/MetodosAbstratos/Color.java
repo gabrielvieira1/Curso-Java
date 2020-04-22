acf4eddffff27006f874ace6fc2f82b6d99d47df
Gabriel Vieira <gabrielvieira1@protonmail.com>
2020-04-21 22:01:18 -0300

    Metodos Abstratos

---
+++ Curso Java/src/sessao14/MetodosAbstratos/Color.java
@@ -0,0 +1,7 @@
+package sessao14.MetodosAbstratos;
+
+public enum Color {
+	BLACK,
+	BLUE,
+	RED;
+}
---
+++ Curso Java/src/sessao14/MetodosAbstratos/Rectangle.java
@@ -0,0 +1,41 @@
+package sessao14.MetodosAbstratos;
+
+
+public class Rectangle extends Shape{
+	
+	private double width;
+	private double height;
+	
+	public Rectangle() {
+		super();
+	}
+
+	public Rectangle(Color color, double width, double height) {
+		super(color);
+		this.width = width;
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	@Override
+	public double area() {
+		return width * height;
+	}
+	
+	
+}
